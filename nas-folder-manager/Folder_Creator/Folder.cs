7bc8bf4ebabe94a03a0c7b2162663a1ad2904c91
Quentin Buache <quentin.buache@unil.ch>
2013-06-18 14:45:13 +0200

    Folder reorganization

---
+++ nas-folder-manager/Folder_Creator/Folder.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Folder_Creator
+{
+    public class Folder
+    {
+        public string Name { get; set; }
+        public bool CreateContent { get; set; }
+        public string State { get; set; }
+        public string StatePerso { get; set; }
+        public string StatePublic { get; set; }
+        public string StatePrivate { get; set; }
+    }
+}
---
+++ nas-folder-manager/Folder_Creator/StreamProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Folder_Creator
+{
+    class StreamProvider
+    {
+        public static List<Folder> GetListOfFolders(string file)
+        {
+            List<Folder> listeFolders = new List<Folder>();
+            try
+            {
+                StreamReader sr = new StreamReader(file, Encoding.UTF8);
+                string line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    Folder folder = new Folder();
+                    folder.Name = line;
+                    folder.CreateContent = true;
+                    folder.State = "En attente";
+                    folder.StatePerso =   "Personnel : En attente";
+                    folder.StatePrivate = "Private : En attente";
+                    folder.StatePublic =  "Public : En attente";
+
+                    listeFolders.Add(folder);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+
+                return listeFolders;
+            }
+            catch
+            {
+                return new List<Folder>();
+            }
+        }
+
+        public static List<string> GetListOfDepartements()
+        {
+            List<string> listeDepartements = new List<string>();
+            try
+            {
+                StreamReader sr = new StreamReader(@".\liste_departements.txt", Encoding.UTF8);
+                string line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    listeDepartements.Add(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+
+                return listeDepartements;
+            }
+            catch
+            {
+                return new List<string>();
+            }
+        }
+    }
+}
